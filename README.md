9ec7aec0fba44a0356791e33c0332432144a6786
Maulid Fajar <67719670+MaulidFajar@users.noreply.github.com>
2024-11-13 13:38:21 +0700

    commit project time-tracking

---
+++ README.md
@@ -0,0 +1,48 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshot
+
+![](./Screenshot.png)
+
+
+### Links
+
+- Solution URL: [Click Here](https://github.com/MaulidFajar/time-tracking)
+- Live Site URL: [Add live site URL here](https://maulidfajar.github.io/user-time-tracking)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Github - [MaulidFajar](https://www.github.com/MaulidFajar)
+- Frontend Mentor - [@MaulidFajar](https://www.frontendmentor.io/profile/MaulidFajar)
---
+++ main.js
@@ -0,0 +1,113 @@
+const cardWrapper = document.querySelector('.card-wrapper');
+const itemLink = document.querySelectorAll('.item');
+
+let userData = [];
+
+fetch('data.json')
+.then((response) => {
+  return response.json();
+})
+.then(data => {
+  userData = data;
+  let cards = ''
+  userData.map(d => {
+    cards += `
+      <div class="card ${d.title.toLowerCase()}">
+        <div class="card-type-name">
+          <span>${d.title}</span>
+          <img src="images/icon-ellipsis.svg">
+        </div>
+        <div class="card-info">
+          <h3>${d.timeframes.weekly.current}hrs</h3>
+          <p>Last Week - ${d.timeframes.weekly.previous}hrs</p>
+        </div>
+      </div>`
+  });
+  cardWrapper.innerHTML = cards;
+})
+
+itemLink.forEach(item => {
+  item.addEventListener('click', () => {
+    itemLink.forEach(button => button.classList.remove('active'));
+    item.classList.add('active');
+
+    if(item.id === 'daily') {
+      updateDaily();
+    } else if (item.id === 'weekly') {
+      updateWeekly();
+    } else if (item.id === 'monthly'){
+      updateMonthly();
+    }
+
+  function updateDaily() {
+    let cards = ''
+      userData.map(d => {
+        cards += `<div class="card ${d.title.toLowerCase()}">
+        <div class="card-type-name">
+          <span>${d.title}</span>
+          <img src="images/icon-ellipsis.svg">
+        </div>
+        <div class="card-info">
+          <h3>${d.timeframes.daily.current}hrs</h3>
+          <p>Last Day - ${d.timeframes.daily.previous}hrs</p>
+        </div>
+      </div>`
+      });
+      cardWrapper.innerHTML = cards;
+  }
+
+    function updateWeekly() {
+      let cards = ''
+        userData.map(d => {
+          cards += `<div class="card ${d.title.toLowerCase()}">
+          <div class="card-type-name">
+            <span>${d.title}</span>
+            <img src="images/icon-ellipsis.svg">
+          </div>
+          <div class="card-info">
+            <h3>${d.timeframes.weekly.current}hrs</h3>
+            <p>Last Week - ${d.timeframes.weekly.previous}hrs</p>
+          </div>
+        </div>`
+        });
+        cardWrapper.innerHTML = cards;
+    }
+
+    function updateMonthly() {
+      let cards = ''
+        userData.map(d => {
+          cards += `<div class="card ${d.title.toLowerCase()}">
+          <div class="card-type-name">
+            <span>${d.title}</span>
+            <img src="images/icon-ellipsis.svg">
+          </div>
+          <div class="card-info">
+            <h3>${d.timeframes.monthly.current}hrs</h3>
+            <p>Last Month - ${d.timeframes.monthly.previous}hrs</p>
+          </div>
+        </div>`
+        });
+        cardWrapper.innerHTML = cards;
+    }
+  })
+})
+
+
+
+
+// function updateUIDaily(data) {
+//   let cards = ''
+//   data.forEach(d => {
+//     cards += `  <div class="card ${d.title.toLowerCase()}">
+//     <div class="card-type-name">
+//       <span>${d.title}</span>
+//       <img src="images/icon-ellipsis.svg">
+//     </div>
+//     <div class="card-info">
+//       <h3>${d.timeframes.daily.current}hrs</h3>
+//       <p>Last Daily - ${d.timeframes.daily.previous}hrs</p>
+//     </div>
+//   </div>`
+//   });
+//   cardWrapper.innerHTML = cards;
+// }
